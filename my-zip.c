a0a208e60ad30e25e43af0f9ee2d43d9929ab510
84765 <liisa.maria.hovilehto@gmail.com>
2025-04-14 15:19:44 +0300

    my-zip

--- my-zip.c
+++ my-zip.c
@@ -1,3 +1,6 @@
+//Source
+//https://www.demo2s.com/g/c/how-to-create-a-zip-file-in-c.html
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -10,3 +13,22 @@
 // n tyypistä merkkiä peräkkäin
 // stduot, joka on kuten stdint kun ohjelma käynnissä
 // kurssilla löytyy
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf("my-zip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    FILE *file;
+
+    for (int i = 1; i < argc; i++) {
+        file = fopen(argv[i], "r");
+        if (file == NULL) {
+            perror("my-zip: cannot open file\n");
+            exit(1);
+        }
+
+    return(0);
+}
