d751afefc86232463fb45d946cde5074c8798e57
84765 <liisa.maria.hovilehto@gmail.com>
2025-04-17 16:08:53 +0300

    zip ja unzip

--- my-unzip.c
+++ my-unzip.c
@@ -1,10 +1,47 @@
+//Source
+//https://www.demo2s.com/g/c/how-to-create-a-zip-file-in-c.html
+//https://stackoverflow.com/questions/17598572/how-to-read-write-a-binary-file
+//https://stackoverflow.com/questions/4358728/end-of-file-eof-in-c
+//https://www.geeksforgeeks.org/fgetc-fputc-c/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
-#include <sys/types.h>
 
-// lukee pakatun tiedosn RLE 
-// fread() funktiolla 
-// tulostaa sen prinf komennolla
-// 
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf("my-unzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    FILE *file;
+
+    for (int i = 1; i < argc; i++) {
+        file = fopen(argv[i], "r");
+        if (file == NULL) {
+            perror("my-unzip: cannot open file\n");
+            exit(1);
+        }
+
+        int count = 0;
+
+        while (fread(&count, sizeof(int), 1, file) == 1) {
+            
+            int charecter = fgetc(file);
+
+            if (charecter == EOF) {
+                fprintf(stderr, "my-unzip: unexpected end of file\n");
+                exit(1);
+            }
+
+            for (int j = 0; j < count; j++) {
+                putchar(charecter);
+            }
+        }
+
+        fclose(file);
+    }
+
+    return(0);
+}
--- my-zip.c
+++ my-zip.c
@@ -1,18 +1,12 @@
 //Source
 //https://www.demo2s.com/g/c/how-to-create-a-zip-file-in-c.html
+//https://stackoverflow.com/questions/17598572/how-to-read-write-a-binary-file
+//https://stackoverflow.com/questions/4358728/end-of-file-eof-in-c
+//https://www.geeksforgeeks.org/fgetc-fputc-c/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
-#include <sys/types.h>
-
-// pakkaus työkalu
-// yksinkertainen
-// rle coding esim aaaab _> 4a1b
-// n tyypistä merkkiä peräkkäin
-// stduot, joka on kuten stdint kun ohjelma käynnissä
-// kurssilla löytyy
 
 int main(int argc, char *argv[]) {
 
@@ -23,13 +17,36 @@ int main(int argc, char *argv[]) {
 
     FILE *file;
 
+    int currentChar = EOF;
+    int previousChar = EOF;
+    int count = 0;
+
     for (int i = 1; i < argc; i++) {
         file = fopen(argv[i], "r");
         if (file == NULL) {
             perror("my-zip: cannot open file\n");
             exit(1);
         }
-    }        
+
+        while ((currentChar = fgetc(file)) != EOF) {
+            if (currentChar == previousChar) {
+                count++;
+            } else {
+                if (previousChar != EOF) {
+                    fwrite(&count, sizeof(int), 1, stdout);
+                    fputc(previousChar, stdout);
+                }
+                count = 1;
+                previousChar = currentChar;
+            }
+        }
+        fclose(file);
+    }
+
+    if (previousChar != EOF) {
+        fwrite(&count, sizeof(int), 1, stdout);
+        fputc(previousChar, stdout);
+    }
 
     return(0);
 }
